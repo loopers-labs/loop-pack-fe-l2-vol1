f81bf98fa759462a3cfa68147ee719d648a5f4ea
auddnjs2008 <auddnjs2008@naver.com>
2026-06-15 23:53:11 +0900

    chore: ESLint 와 Prettier 설정 추가

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig(
+  {
+    ignores: ["dist", "node_modules"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "error",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/ban-ts-comment": "error",
+      "no-empty": "error",
+      "no-console": ["error", { allow: ["warn", "error"] }],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      curly: ["error", "all"],
+      "no-implicit-coercion": "error",
+      "no-unneeded-ternary": "error",
+    },
+    eslintConfigPrettier,
+  }
+);
