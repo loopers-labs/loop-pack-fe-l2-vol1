26a6e3f7d2ecb001dcb9141bf4a965d05f82f2b9
kate-son <yenihand13@gmail.com>
2026-06-17 04:00:46 +0900

    chore: prettier package.json에 설치.
    - eslint.config.js에 prettier 추가 (충돌 방지)

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,20 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
-import { defineConfig } from "eslint/config";
+import prettier from "eslint-config-prettier"
+import {defineConfig} from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        plugins: {js},
+        extends: ["js/recommended"],
+        languageOptions: {globals: globals.browser}
+    },
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
     reactHooks.configs.flat["recommended-latest"],
+    prettier,
     {
         rules: {
             "react/react-in-jsx-scope": "off", // React 17+ 에서는 불필요
