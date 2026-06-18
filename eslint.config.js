46df39ab954f86995a7990756293febde874eb7f
kate-son <yenihand13@gmail.com>
2026-06-19 02:08:41 +0900

    chore: eslint import 규칙 추가
    - import React from 'react' 사용 금지

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,7 @@ export default defineConfig([
             "react/react-in-jsx-scope": "off", // React 17+ 에서는 불필요
             "react-hooks/rules-of-hooks": "error",
             "react-hooks/exhaustive-deps": "error",
+            "@typescript-eslint/consistent-type-imports": "error",
         }
     }
 ]);
