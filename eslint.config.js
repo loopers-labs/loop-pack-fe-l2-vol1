7fedc94cd2e084407afe6cfd8aa0635588b6076d
auddnjs2008 <auddnjs2008@naver.com>
2026-06-22 07:18:29 +0900

    chore: React ESLint 보안 규칙 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
@@ -19,6 +20,7 @@ export default defineConfig(
       globals: globals.browser,
     },
     plugins: {
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
@@ -40,6 +42,9 @@ export default defineConfig(
       curly: ["error", "all"],
       "no-implicit-coercion": "error",
       "no-unneeded-ternary": "error",
+
+      "react/no-danger": "error",
+      "react/jsx-no-target-blank": "error",
     },
   },
   eslintConfigPrettier,
