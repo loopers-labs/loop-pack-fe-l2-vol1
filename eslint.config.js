89129fba60fa512d31ebe8a48ca15e55d5738f5a
im-wen3y <imwen3y@gmail.com>
2026-06-21 20:35:44 +0900

    chore: exhaustive-deps error 근거 주석 추가

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,10 @@ export default [
     extends: ['plugin:prettier/recommended'],
     rules: {
       'react-hooks/rules-of-hooks': 'error',
+      /**
+       * deps 누락 시 eslint-disable + 사유 주석을 강제
+       * deps에서 빼기보다 의존을 안 하는 형태로 코드를 바꿔 stale closure 방지
+       */
       'react-hooks/exhaustive-deps': 'error',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-floating-promises': 'error',
